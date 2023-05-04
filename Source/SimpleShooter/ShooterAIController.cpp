5ac2a04fe023d7b7e9de964de030170e2616b4db
Guillermo Riaño <guilledark17@gmail.com>
2023-05-04 11:22:52 -0600

    ✅ BT Decorators And Selectors

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -19,11 +19,7 @@ void AShooterAIController::BeginPlay()
         APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
         // Set StartLocation
-        GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
-
-
-        // Adding a new Key to Blackboard of type Vector
-        GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+        GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());        
     }
         
 }
@@ -33,19 +29,19 @@ void AShooterAIController::Tick(float DeltaSeconds)
     Super::Tick(DeltaSeconds);
 
     // Get the player pawn
-    // APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
-    // if (LineOfSightTo(PlayerPawn))
-    // {
-    //     // Set Focus a our player
-    //     SetFocus(PlayerPawn);    
-
-    //     // Move AI to our player
-    //     MoveToActor(PlayerPawn, AcceptanceRadius);        
-    // }   
-    // else
-    // {
-    //     ClearFocus(EAIFocusPriority::Gameplay);
-    //     StopMovement();
-    // }
+    if (LineOfSightTo(PlayerPawn))
+    {
+        // Adding a new Key to Blackboard of type Vector - Setting PLayerLocation
+        GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
+        
+        // Setting LastKnow
+        GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnowPlayerLocation"), PlayerPawn->GetActorLocation());
+    }   
+    else
+    {
+        // Clear PLayerLocation
+        GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
+    }
 }
