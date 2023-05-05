15981ab24aa8469a9451b0a647cadace9a26192f
Guillermo Riaño <guilledark17@gmail.com>
2023-05-05 11:19:49 -0600

    ✅ Executing BTTasks

--- Source/SimpleShooter/BTTask_ClearBlackboardValue.cpp
+++ Source/SimpleShooter/BTTask_ClearBlackboardValue.cpp
@@ -2,9 +2,21 @@
 
 
 #include "BTTask_ClearBlackboardValue.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 
 UBTTask_ClearBlackboardValue::UBTTask_ClearBlackboardValue()
 {
+    // Create a new node
     NodeName = "Clear Blackboard Value";
+}
+
+EBTNodeResult::Type UBTTask_ClearBlackboardValue::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    Super::ExecuteTask(OwnerComp, NodeMemory);
+
+    // Get Blackboard key of the ownercomp
+    OwnerComp.GetBlackboardComponent()->ClearValue(GetSelectedBlackboardKey());
+
+    return EBTNodeResult::Succeeded;
 }
