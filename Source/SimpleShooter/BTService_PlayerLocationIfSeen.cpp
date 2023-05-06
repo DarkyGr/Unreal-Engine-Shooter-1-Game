d4cb99440dec1998f2c16339d07eaa952085179c
Guillermo Riaño <guilledark17@gmail.com>
2023-05-06 12:13:10 -0600

    ✅ BTServices in C++ 2

---
+++ Source/SimpleShooter/BTService_PlayerLocationIfSeen.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTService_PlayerLocationIfSeen.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Pawn.h"
+#include "AIController.h"
+
+UBTService_PlayerLocationIfSeen::UBTService_PlayerLocationIfSeen()
+{
+    NodeName = "Player Location If Seen";
+}
+
+void UBTService_PlayerLocationIfSeen::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+    Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+    // Get Ans set Pawn of the world
+    APawn *PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    if (PlayerPawn == nullptr)
+    {
+        return;
+    }    
+
+    // Check if the AI Owner is null
+    if (OwnerComp.GetAIOwner() == nullptr)
+    {
+        return;
+    }
+    
+    // Check the Line
+    if (OwnerComp.GetAIOwner()->LineOfSightTo(PlayerPawn))
+    {
+        // Set To Owner Component the new Key
+        OwnerComp.GetBlackboardComponent()->SetValueAsVector(GetSelectedBlackboardKey(), PlayerPawn->GetActorLocation());        
+    }
+    else
+    {
+        OwnerComp.GetBlackboardComponent()->ClearValue(GetSelectedBlackboardKey());
+    }   
+
+}
